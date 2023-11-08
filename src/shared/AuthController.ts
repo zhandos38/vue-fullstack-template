f808fae578b37318d2d13463b8abcd183cfa5185
jake <zh.timabay@mycar.kz>
2023-11-08 13:50:48 +0600

    init

---
+++ src/shared/AuthController.ts
@@ -0,0 +1,17 @@
+import { BackendMethod } from 'remult'
+import { Roles } from './Role'
+
+export class AuthController {
+  @BackendMethod({ allowed: true })
+  static async signIn(username: string) {
+    const validUsers = [
+      { id: '1', name: 'Jane', roles: [Roles.admin] },
+      { id: '2', name: 'Steve', roles: [Roles.user] },
+      { id: '3', name: 'John', roles: [] }
+    ]
+    const user = validUsers.find((user) => user.name === username)
+
+    if (!user) throw new Error("Invalid user, try 'Steve' or 'Jane'")
+    return (await import('jsonwebtoken')).sign(user, process.env['JWT_SECRET'] || 'my secret')
+  }
+}
